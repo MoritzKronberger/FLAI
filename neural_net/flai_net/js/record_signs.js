b0722b8ee128da571ff8515dadea41e3cfdcc30e
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-11-12 15:46:26 +0100

    Add labeled image capturing

--- neural_net/flai_net/js/record_signs.js
+++ neural_net/flai_net/js/record_signs.js
@@ -17,9 +17,38 @@ async function loadWebcamVideo() {
   });
 }
 
-async function main() {
-    const video = await loadWebcamVideo()
-    console.log(video)
+// derived from https://stackoverflow.com/a/13765373/14906871
+function mirrorToCanvas(video) {
+  let canvas = document.createElement("canvas");
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  let canvasContext = canvas.getContext("2d");
+  canvasContext.drawImage(video, 0, 0, canvas.width, canvas.height);
+  return canvas;
 }
 
-main()
+function saveLabeledCapture(video, captures, label, labelNum) {
+  const canvas = mirrorToCanvas(video);
+  const image = canvas.toDataURL("image/jpg");
+  captures[`${label}_${labelNum}`] = image;
+  console.log(captures);
+}
+
+async function main() {
+  const video = await loadWebcamVideo();
+  const letters = [..."abcdefghiklmnopqrstuvwxy"];
+  let labels = {};
+  letters.forEach((letter) => {
+    labels[letter] = 0;
+  });
+  let captures = {};
+  document.addEventListener("keypress", (event) => {
+    const key = event.key;
+    if (letters.includes(key)) {
+      saveLabeledCapture(video, captures, key, labels[key]);
+      labels[key]++;
+    }
+  });
+}
+
+main();
