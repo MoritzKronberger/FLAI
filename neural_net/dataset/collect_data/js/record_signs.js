67aa71f640e4337bfa702d1e55cbd43922ea0670
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-11-12 16:12:55 +0100

    Add label count statistic

--- neural_net/dataset/collect_data/js/record_signs.js
+++ neural_net/dataset/collect_data/js/record_signs.js
@@ -18,21 +18,30 @@ async function loadWebcamVideo() {
 }
 
 // derived from https://stackoverflow.com/a/13765373/14906871
-function mirrorToCanvas(video) {
+const mirrorToCanvas = (video) => {
   let canvas = document.createElement("canvas");
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   let canvasContext = canvas.getContext("2d");
   canvasContext.drawImage(video, 0, 0, canvas.width, canvas.height);
   return canvas;
-}
+};
 
-function saveLabeledCapture(video, captures, label, labelNum) {
+const saveLabeledCapture = (video, captures, label, labelNum) => {
   const canvas = mirrorToCanvas(video);
   const image = canvas.toDataURL("image/jpg");
   captures[`${label}_${labelNum}`] = image;
   console.log(captures);
-}
+};
+
+const updateStatistic = (labels, statistic) => {
+  statistic.innerHTML = "";
+  for (let label of Object.entries(labels)) {
+    const li = document.createElement("li");
+    li.innerText = `${label[0]}: ${label[1]}`;
+    statistic.appendChild(li);
+  }
+};
 
 async function main() {
   const video = await loadWebcamVideo();
@@ -42,11 +51,14 @@ async function main() {
     labels[letter] = 0;
   });
   let captures = {};
+  let statistic = document.querySelector("#labelStatistic");
+  updateStatistic(labels, statistic);
   document.addEventListener("keypress", (event) => {
     const key = event.key;
     if (letters.includes(key)) {
       saveLabeledCapture(video, captures, key, labels[key]);
       labels[key]++;
+      updateStatistic(labels, statistic);
     }
   });
 }
