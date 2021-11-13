b4c86536fcfe8044dd0b727ec7fd45c29c894882
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-11-13 17:49:24 +0100

    Cleanup

--- neural_net/dataset/collect_data/js/record_signs.js
+++ neural_net/dataset/collect_data/js/record_signs.js
@@ -1,7 +1,6 @@
-async function loadWebcamVideo() {
+const loadWebcamVideo = async () => {
   // from https://github.com/tensorflow/tfjs-models/blob/master/face-landmarks-detection/demo/index.js
   const video = document.querySelector("#webcamVideo");
-
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: {
@@ -15,31 +14,31 @@ async function loadWebcamVideo() {
       resolve(video);
     };
   });
-}
+};
 
-// derived from https://stackoverflow.com/a/13765373/14906871
-const mirrorToCanvas = (video) => {
-  let canvas = document.createElement("canvas");
+const createCanvas = (video) => {
+  const canvas = document.createElement("canvas");
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
-  let canvasContext = canvas.getContext("2d");
-  canvasContext.drawImage(video, 0, 0, canvas.width, canvas.height);
   return canvas;
 };
 
-const saveLabeledCapture = (video, captures, label, labelNum) => {
-  const canvas = mirrorToCanvas(video);
+const mirrorToCanvas = (canvas, video) => {
+  canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+};
+
+const saveLabeledCapture = async (video, canvas, label) => {
+  // image saving via canvas derived from https://stackoverflow.com/a/13765373/14906871
+  mirrorToCanvas(canvas, video);
   const image = canvas.toDataURL("image/png");
-  captures[`${label}_${labelNum}`] = image;
-  axios({
-    method: "post",
-    url: "http://localhost:3000/examples",
-    data: {
+  try {
+    await axios.post("http://localhost:3000/examples/", {
       label: label,
       image: image,
-    },
-  });
-  console.log(captures);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const updateStatistic = (labels, statistic) => {
@@ -50,7 +49,7 @@ const updateStatistic = (labels, statistic) => {
     const th = document.createElement("th");
     const td = document.createElement("td");
     th.innerText = label;
-    td.innerText = number
+    td.innerText = number;
     tr_th.appendChild(th);
     tr_td.appendChild(td);
   }
@@ -58,20 +57,23 @@ const updateStatistic = (labels, statistic) => {
   statistic.appendChild(tr_td);
 };
 
-async function main() {
+const main = async () => {
   const video = await loadWebcamVideo();
   const letters = [..."abcdefghiklmnopqrstuvwxy"];
   let labels = {};
-  let captures = {};
   letters.forEach((letter) => {
     labels[letter] = 0;
   });
-  let statistic = document.querySelector("#labelStatistic");
+
+  const statistic = document.querySelector("#labelStatistic");
   updateStatistic(labels, statistic);
-  document.addEventListener("keypress", (event) => {
+
+  const canvas = createCanvas(video);
+
+  document.addEventListener("keypress", async (event) => {
     const key = event.key;
     if (letters.includes(key)) {
-      saveLabeledCapture(video, captures, key, labels[key]);
+      await saveLabeledCapture(video, canvas, key);
       labels[key]++;
       updateStatistic(labels, statistic);
     }
