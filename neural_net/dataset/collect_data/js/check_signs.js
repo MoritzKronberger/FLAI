556e32d63660c1ff2d394e09dd21700176d96086
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-11-13 17:08:32 +0100

    Add check_signs key controls

--- neural_net/dataset/collect_data/js/check_signs.js
+++ neural_net/dataset/collect_data/js/check_signs.js
@@ -1,25 +1,35 @@
 const url = "http://localhost:3000/examples/";
-var data;
-var frame = document.getElementById("image");
-var current_frame = 0;
+let data;
+let frame = document.getElementById("image");
+let current_frame = 0;
 
-axios
-  .get(url)
-  .then((response) => {
-    data = response.data;
-    loadImages(data)
-  })
+axios.get(url).then((response) => {
+  data = response.data;
+  loadImages(data);
+});
 
 function loadImages(data) {
   frame.src = data[current_frame].image;
 }
 
-function deleteFrame()
-{
-    let frameUrl = url + data[current_frame].id;
-    axios
-    .delete(frameUrl)
-    .then((response) => {
-        console.log(response);
-      })
+document.addEventListener("keydown", (e) => {
+  if (e.key == "s") {
+    current_frame++;
+  } else if (e.key == "ArrowLeft" && current_frame >= 0) {
+    current_frame--;
+  }
+  loadImages(data);
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "d") {
+    deleteFrame();
+  }
+});
+
+function deleteFrame() {
+  let frameUrl = url + data[current_frame].id;
+  axios.delete(frameUrl).then((response) => {
+    console.log(response);
+  });
 }
