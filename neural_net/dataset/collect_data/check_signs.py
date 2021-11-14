509e99eb4486b803a8bfee1cf67fe1345bc0034f
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-11-14 16:50:54 +0100

    Fix check_signs path

--- neural_net/dataset/collect_data/check_signs.py
+++ neural_net/dataset/collect_data/check_signs.py
@@ -11,22 +11,19 @@ def start_recording(dir, dataset_dir):
     font_size = .6
     font_thickness = 1
 
-    #reference = cv2.imread('./references/alphabet.png')
-    #reference_width = reference.shape[1]
-    #reference_height = reference.shape[0]
-
+    
     current_frame = 0
 
     while True:
         try:
-
             frame = cv2.imread(dir[current_frame])
             frame_width = frame.shape[1]
             frame_height = frame.shape[0]
 
             label = dir[current_frame].replace(dataset_dir, '')[1]
-            ref = cv2.imread('./references/single_signs/' + label + '.png')
 
+            #ref = cv2.imread('./references/single_signs/' + label + '.png')
+            ref = cv2.imread('./references/single_signs/b.png')
             ref_width = ref.shape[1]
             ref_height = ref.shape[0]
 
@@ -48,7 +45,7 @@ def start_recording(dir, dataset_dir):
 
             cv2.putText(ref,
                         'Label: ' + label,
-                        (30, int(frame_height - 0)),
+                        (30, int(ref_height - 10)),
                         font,
                         font_size,
                         font_color,
@@ -60,17 +57,16 @@ def start_recording(dir, dataset_dir):
             keypress = cv2.waitKeyEx(1)
 
             if not keypress == -1:
-                if keypress == 115: #s
+                if keypress == 115:  # s
                     current_frame += 1
-                elif keypress == 100: #d
+                elif keypress == 100:  # d
                     os.remove(dir[current_frame])
                     dir.remove(dir[current_frame])
                     print('delete ' + str(current_frame))
-                elif keypress == 2424832: #arrow left
-                    current_frame-=1
-                elif keypress == 2555904: #arrow right
-                    current_frame+=1
-
+                elif keypress == 2424832:  # arrow left
+                    current_frame -= 1
+                elif keypress == 2555904:  # arrow right
+                    current_frame += 1
 
             if keypress == 27:  # esc
                 break
@@ -87,25 +83,22 @@ def loadAllPaths(dataset_dir):
     os.chdir(dataset_dir)
     label_dirs = os.listdir()
     for dir in label_dirs:
-        path = os.path.join(dataset_dir, dir)
-        os.chdir(path)
+        path_to_example = os.path.join(dataset_dir, dir)
+        os.chdir(path_to_example)
         examples = os.listdir()
         for example in examples:
-            path = os.path.join(path, example)
+            path = os.path.join(path_to_example, example)
+            print(path)
             dirs.append(path)
     return dirs
 
 
 def main():
-
-    labels = list(settings.labels)
     dataset_dir = settings.dataset_directory
-    # start_recording(dataset_dir, labels, image_format)
     dirs = loadAllPaths(dataset_dir)
     dir_path = os.path.dirname(os.path.realpath(__file__))
     os.chdir(dir_path)
     start_recording(dirs, dataset_dir)
-
 
 if __name__ == '__main__':
     main()
