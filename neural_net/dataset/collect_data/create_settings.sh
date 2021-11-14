05ae20af2b294570ba19816a2e9878a63a0b7c78
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-11-14 14:13:50 +0100

    Update settings.py

--- neural_net/dataset/collect_data/create_settings.sh
+++ neural_net/dataset/collect_data/create_settings.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "labels = 'abcdefghiklmnopqrstuvwxy'"> settings.py
-echo "dataset_directory = ''" >> settings.py
+echo "dataset_directory = r''" >> settings.py
 echo "image_format = '.jpg'" >> settings.py
-echo "camera = 1" >> settings.py
+echo "camera = 0" >> settings.py
 
 echo "------------> $0 has finished successfully"
