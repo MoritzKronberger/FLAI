eff259132e29b52332ddd1fbe6f5349b2e7d24c2
jelllea <lea.jell@hs-augsburg.de>
2021-11-15 18:46:23 +0100

    Update syntax of HelloWorld setup

--- frontend/flai_app/src/components/HelloWorld.vue
+++ frontend/flai_app/src/components/HelloWorld.vue
@@ -1,11 +1,3 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
   <h1>{{ msg }}</h1>
 
@@ -32,6 +24,24 @@ const count = ref(0)
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
+
+<script lang="ts">
+import Vue, { ref, defineComponent } from 'vue';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  data() {
+    let count: number;
+    return {
+      count: 0,
+    }
+  },
+
+}
+</script>
 
 <style scoped>
 a {
