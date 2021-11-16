3d3315cd7b69cf724b0e7fd5012904fbe0b1b5b4
jelllea <lea.jell@hs-augsburg.de>
2021-11-16 15:02:00 +0100

    Add rules for eslint

--- frontend/flai_app/.eslintrc.js
+++ frontend/flai_app/.eslintrc.js
@@ -2,5 +2,30 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
+  globals: {
+    module: false,
+  },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    indent: ["error", 2],
+    "no-multiple-empty-lines": "warn",
+    properties: "always", // Checks for lowerCamelCase
+    quotes: ["error", "single"],
+    "no-var": "error",
+    "prefer-const": [
+      "error",
+      {
+        destructuring: "all", //Checks for destructured variables, where all should be const. Can be changed to "any"
+        ignoreReadBeforeAssign: false,
+      },
+    ],
+    "no-use-before-define": "error",
+    eqeqeq: [
+      "error",
+      "always",
+      {
+        null: "ignore",
+      },
+    ],
+  },
 };
