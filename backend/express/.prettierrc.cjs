b03976b8a7a3b89c9ddeeb450a1f2f460bf0bbaa
jelllea <lea.jell@hs-augsburg.de>
2021-11-17 13:58:43 +0100

    Add prettier rules and prettierignore as in vue

---
+++ backend/express/.prettierrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  semi: false,
+  trailingComma: "es5",
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  bracketSpacing: true,
+  overrides: [
+    {
+      files: ".js",
+      options: {
+        parser: "javascript"
+      }
+    },
+    {
+      files: ".ts",
+      options: {
+        parser: "typescript"
+      }
+    }
+  ]
+}
+  
