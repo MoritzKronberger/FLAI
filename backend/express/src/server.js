eb8f6276f6ae144e8dc67770a9499f8dc2061e7a
jelllea <lea.jell@hs-augsburg.de>
2021-11-17 13:43:12 +0100

    Test husky on server.js

--- backend/express/src/server.js
+++ backend/express/src/server.js
@@ -1,8 +1,8 @@
 // hello world from https://expressjs.com/de/starter/hello-world.html
 import express from 'express'
 
-const app = express()
-const port = process.env.PORT
+let app = express()
+  const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
