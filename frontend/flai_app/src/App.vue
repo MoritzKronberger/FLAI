9b74b7b51541c4bb226abaf6b8dcda4f94a2581b
Kieu Pham <kieu.pham@hs-augsburg.de>
2021-12-17 12:52:48 +0100

    create input field

--- frontend/flai_app/src/App.vue
+++ frontend/flai_app/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Inputfield from './components/InputField.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hallo, wir sind FLAI!" />
+  <!--<HelloWorld msg="Hallo, wir sind FLAI!" />-->
+  <Inputfield />
 </template>
 
 <style>
---
+++ frontend/flai_app/src/components/InputField.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <input
+      v-model="newInput"
+      type="text"
+      placeholder="Placeholder Text"
+      @keyup.enter="addInput"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ref } from 'vue'
+
+export default defineComponent({
+  name: 'InputField',
+  setup() {
+    const newInput = ref<string>('')
+    const inputData = ref('')
+
+    console.log(inputData)
+
+    function addInput() {
+      if (newInput.value !== '') {
+        inputData.value = newInput.value
+        console.log(inputData)
+      }
+    }
+
+    return { newInput, inputData, addInput }
+  },
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
