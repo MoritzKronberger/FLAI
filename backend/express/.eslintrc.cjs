20b2624cd08a4881b715c2e83ab990eee68228fb
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-17 15:19:38 +0100

    Add parser to eslint

--- backend/express/.eslintrc.cjs
+++ backend/express/.eslintrc.cjs
@@ -2,37 +2,38 @@ module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: [
-    "prettier"
-  ],
-  extends: [
-    "eslint:recommended",
-    "plugin:prettier/recommended"
-  ],
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   globals: {
-      module: false
+    module: false,
   },
+  parser: '@babel/eslint-parser',
   rules: {
-    "indent": ["error", 2],
-    "no-multiple-empty-lines": "warn",
-    "camelcase": "error", // Checks for lowerCamelCase
-    "quotes": ["error", "single"],
-    "no-var": "error",
-    "prefer-const": [
-      "error", {
-        destructuring: "all", //Checks for destructured variables, where all should be const. Can be changed to "any"
+    indent: ['error', 2],
+    'no-multiple-empty-lines': 'warn',
+    camelcase: 'error', // Checks for lowerCamelCase
+    quotes: ['error', 'single'],
+    'no-var': 'error',
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all', //Checks for destructured variables, where all should be const. Can be changed to "any"
         ignoreReadBeforeAssign: false,
       },
     ],
-    "no-use-before-define": "error",
-    "eqeqeq": [ "error", "always", {
-      null: "ignore" 
-    }],
-    "prettier/prettier": "error"
-  }
+    'no-use-before-define': 'error',
+    eqeqeq: [
+      'error',
+      'always',
+      {
+        null: 'ignore',
+      },
+    ],
+    'prettier/prettier': 'error',
+  },
 }
