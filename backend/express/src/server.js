a127e13fd01e31c53cf020f6fb2ff498742cf015
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-17 11:01:29 +0100

    Update server.js

--- backend/express/src/server.js
+++ backend/express/src/server.js
@@ -4,10 +4,6 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`FLAI app listening at http://localhost:${port}`)
 })
