555054ca6246e8c80b6e23c768751b9366cbe0d4
Zelastion <zelastion@gmail.com>
2021-12-17 12:15:59 +0100

    test commit wegen fehler

--- frontend/flai_app/src/App.vue
+++ frontend/flai_app/src/App.vue
@@ -6,7 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hallo, wir sind FLAI!" />
+  <HelloWorld msg="Hallo, wir sind cool!" />
 </template>
 
 <style>
