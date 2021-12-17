ce9a93daa031668bfc2c2be40cf515923cfd6c39
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-12-17 17:47:20 +0100

    Add insertion into includes_sign to populate_spelling_excercise

--- backend/postgres_db/sql/99_insert_data.sql
+++ backend/postgres_db/sql/99_insert_data.sql
@@ -54,7 +54,9 @@ VALUES
  (SELECT "id" FROM excercise WHERE "name"='Buchstabieren lernen')
 );
 
-/* populate_spelling_excercise: inserts a sign for each letter provided in the alphabet string */
+/* populate_spelling_excercise: */
+/* inserts a sign for each letter provided in the alphabet string */
+/* and adds the sign to both the 'AI Feedback' and 'Memory' task */
 CREATE OR REPLACE FUNCTION populate_spelling_excercise(_alphabet TEXT, _motion_category TEXT)
     RETURNS VOID
 LANGUAGE plpgsql
@@ -65,9 +67,22 @@ $$
     BEGIN
         _letters := REGEXP_SPLIT_TO_ARRAY(_alphabet, '');
         FOREACH _letter IN ARRAY _letters LOOP
+
             INSERT INTO "sign" ("name", "motion_category_id")
             VALUES
-            (_letter, (SELECT "id" FROM e_motion_category WHERE "name"=_motion_category));
+            (_letter, (SELECT "id" FROM "e_motion_category" WHERE "name"=_motion_category));
+
+            INSERT INTO "includes_sign" ("task_id", "sign_id", "order")
+            VALUES
+            ((SELECT "id" FROM "task" WHERE "name"='AI Feedback'), 
+             (SELECT "id" FROM "sign" WHERE "name"=_letter),
+             NULL
+            ),
+            ((SELECT "id" FROM "task" WHERE "name"='Memory'), 
+             (SELECT "id" FROM "sign" WHERE "name"=_letter),
+             NULL
+            );
+
         END LOOP;
     END
 $$
