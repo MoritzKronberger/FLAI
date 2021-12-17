b9411714c51b22177106d63c43526c9a3d5e536e
Zelastion <zelastion@gmail.com>
2021-12-17 12:25:17 +0100

    Button überarbeitet, ändert jetzt text beim click

--- frontend/flai_app/src/App.vue
+++ frontend/flai_app/src/App.vue
@@ -2,11 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import vbutton from './components/button.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hallo, wir sind cool!" />
+  <vbutton/>
 </template>
 
 <style>
---
+++ frontend/flai_app/src/components/button.vue
@@ -0,0 +1,35 @@
+<template>
+  <input
+    :value="bText"
+    class="vbutton"
+    type="button"
+    @click="doSomething()"
+    /> 
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "Button",
+  props: {bText: String, activeLabel: String},
+  
+  data() {
+    return {
+      bText: "Ich bin ein Button.",
+    }
+  },
+
+  methods: {
+    doSomething() {
+      this.bText = "du hast mich gedrückt!"
+    }
+  }
+});
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
