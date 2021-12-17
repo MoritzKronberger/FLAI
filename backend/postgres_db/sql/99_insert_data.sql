5c4a6c3b0457f9c076ed930d35e8c866eeeb4f9f
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-12-17 15:25:12 +0100

    Add data for e_sort_signs

--- backend/postgres_db/sql/99_insert_data.sql
+++ backend/postgres_db/sql/99_insert_data.sql
@@ -1,4 +1,15 @@
+/*************************************************************************************
+ * Insert required data
+ *************************************************************************************/
+
 BEGIN;
+
+
+/* sort_signs (ways signs can be ordered during an exercise)*/
+INSERT INTO "e_sort_signs" ("name")
+VALUES
+('alphabetical'),
+('occurrence');
 
 
 COMMIT;
