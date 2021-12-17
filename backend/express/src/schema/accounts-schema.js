1a0d4be2e4c4a2f7e8d50feb08ad7b62a18fb97f
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-17 15:45:11 +0100

    Update EOF on Files

--- backend/express/src/schema/accounts-schema.js
+++ backend/express/src/schema/accounts-schema.js
@@ -1,12 +1,12 @@
 /* template: https://gitlab.multimedia.hs-augsburg.de/kowa/wk_account_express_01 */
 
 const accountSchema = {
-    type: "object",
-    properties: {
-      id: { type: "string", format: "uuid" },
-      username: { type: ["string", "null"] },
-      password: { type: ["string", "null"] },
-    },
-  };
+  type: 'object',
+  properties: {
+    id: { type: 'string', format: 'uuid' },
+    username: { type: ['string', 'null'] },
+    password: { type: ['string', 'null'] },
+  },
+}
 
-export default accountSchema;
+export default accountSchema
--- backend/express/src/schema/validate.js
+++ backend/express/src/schema/validate.js
@@ -1,7 +1,7 @@
 /* template: https://gitlab.multimedia.hs-augsburg.de/kowa/wk_account_express_01 */
 
-import { Validator } from "express-json-validator-middleware";
+import { Validator } from 'express-json-validator-middleware'
 
-const { validate } = new Validator({ format: "full" });
+const { validate } = new Validator({ format: 'full' })
 
-export default validate;
+export default validate
