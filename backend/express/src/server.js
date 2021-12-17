0f9d7495b78adeb052881f737be21294a4d94b1a
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-17 15:34:48 +0100

    Update server.js imports and middleware

--- backend/express/src/server.js
+++ backend/express/src/server.js
@@ -1,9 +1,19 @@
 // hello world from https://expressjs.com/de/starter/hello-world.html
 import express from 'express'
-
 const app = express()
-const port = process.env.PORT
 
+import cors from 'cors'
+
+//import routes
+import { exercise } from './routes/exercise.js'
+
+// middleware
+app.use(express.json())
+app.use(cors())
+app.use('/api/exercise', exercise)
+
+// server
+const port = process.env.PORT
 app.listen(port, () => {
   console.log(`FLAI app listening at http://localhost:${port}`)
 })
