664f36f9c0f34833160460093bd48222ee58843f
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-18 18:26:46 +0100

    Add account route

--- backend/express/src/server.js
+++ backend/express/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+
 const app = express()
 
 import cors from 'cors'
@@ -6,11 +7,14 @@ import cors from 'cors'
 //import routes
 import { exercise } from './routes/exercise.js'
 import { signs } from './routes/signs.js'
+import { account } from './routes/accounts.js'
 // middleware
 app.use(express.json())
+
 app.use(cors())
 app.use('/api/exercise', exercise)
 app.use('/api/signs', signs)
+app.use('/api/accounts', account)
 // server
 const port = process.env.PORT
 app.listen(port, () => {
