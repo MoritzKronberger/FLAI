35b547844e42a1df86a8766904e191d8fa07a35a
jelllea <lea.jell@hs-augsburg.de>
2021-12-19 12:54:33 +0100

    Add styling to homepage

--- frontend/flai_app/src/views/HomePage.vue
+++ frontend/flai_app/src/views/HomePage.vue
@@ -23,8 +23,16 @@
   <h2>Statistik</h2>
   <div class="row">
     <div class="statistic">
-      <p class="number">5</p>
+      <div class="circle">
+        <p class="number">5</p>
+      </div>
       <p class="underline">Statistik Platzhalter</p>
+    </div>
+    <div class="statistic">
+      <div class="circle">
+        <p class="number">3</p>
+      </div>
+      <p class="underline">Statistik Platzhalter2</p>
     </div>
   </div>
 </template>
@@ -49,14 +57,42 @@ export default {
 h2 {
   font-weight: 200;
 }
+p {
+  margin: 0;
+}
 .row {
   display: flex;
 }
 .row img {
+  margin: 0 1vw;
   height: 30vh;
-}
-#exercisePicture {
-  margin: 0;
   border-radius: 3vw;
+}
+.statistic {
+  margin: 1vw;
+  padding: 3vh 1vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  background-color: lightblue;
+  border-radius: 2vw;
+  text-align: center;
+}
+.circle {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: yellow;
+  margin: auto;
+  margin-bottom: 2vh;
+  height: 10vh;
+  width: 10vh;
+  border-radius: 50%;
+}
+.number {
+  margin: auto;
+  width: 100%;
+  font-size: 5vh;
 }
 </style>
