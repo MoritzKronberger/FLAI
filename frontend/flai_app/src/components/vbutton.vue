2b5223cbb049cb1520362ee56a39282149e75359
Zelastion <zelastion@gmail.com>
2021-12-19 11:49:36 +0100

    Update button component vbutton.vue

---
+++ frontend/flai_app/src/components/vbutton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ label: string; btnclass: string }>()
+const emit = defineEmits(['click'])
+
+function _click() {
+  emit('click')
+}
+</script>
+
+<template>
+  <input type="button" :value="label" :class="btnclass" @click="_click()" />
+</template>
+
+<style lang="scss" scoped></style>
