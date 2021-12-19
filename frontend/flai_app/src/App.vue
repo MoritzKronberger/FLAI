bdf8bf1369fa0622ee7b39a44510fb107a7a3df4
Zelastion <zelastion@gmail.com>
2021-12-19 11:50:58 +0100

    Added vbutton to App.vue for testing

--- frontend/flai_app/src/App.vue
+++ frontend/flai_app/src/App.vue
@@ -2,13 +2,17 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
-import vbutton from './components/button.vue'
+import vbutton from './components/vbutton.vue'
+
+function ConsoleTest() {
+  console.log('Ich bin ein Test')
+}
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hallo, wir sind cool!" />
-  <vbutton label="test" />
+  <vbutton label="test" btnclass="" @click="ConsoleTest()" />
 </template>
 
 <style>
