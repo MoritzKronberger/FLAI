d080f35f50a873cf7dcddbace3888bb1b17f3392
jelllea <lea.jell@hs-augsburg.de>
2021-12-19 11:27:28 +0100

    Install vuerouter and add index file

---
+++ frontend/flai_app/src/router/index.js
@@ -0,0 +1,10 @@
+import { createWebHistory, createRouter } from 'vue-router'
+
+const routes = []
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
