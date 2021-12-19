de2e4cef48356ad9e845d23dc52e5ffb8380f8b5
ekun <eva.kunzmann@hs-augsburg.de>
2021-12-19 13:17:11 +0100

    Adjust SidebarMenu.vue

--- frontend/flai_app/src/components/SidebarMenu.vue
+++ frontend/flai_app/src/components/SidebarMenu.vue
@@ -1,81 +1,51 @@
+<script setup lang="ts">
+import MenuItems from './MenuItems.vue'
+
+const menuItems = [
+  {
+    link: '#home',
+    icon: 'H',
+    description: 'Home',
+    state: true,
+  },
+  {
+    link: '#training',
+    icon: 'T',
+    description: 'Training',
+    state: false,
+  },
+  {
+    link: '#profile',
+    icon: 'P',
+    description: 'Profil',
+    state: false,
+  },
+  {
+    link: '#stats',
+    icon: 'S',
+    description: 'Lernstatistik',
+    state: false,
+  },
+  {
+    link: '#tutorial',
+    icon: 'T',
+    description: 'Tutorial',
+    state: false,
+  },
+]
+</script>
+
 <template>
   <nav>
     <span v-for="(menuItem, index) in menuItems" :key="index">
-      <li @mouseover="show = true" @mouseleave="show = false">
-        <a :href="menuItem.link">
-          <span class="icon">{{ menuItem.icon }}</span>
-          <span v-if="show">{{ menuItem.description }}</span>
-        </a>
-      </li>
+      <MenuItems
+        :link="menuItem.link"
+        :icon="menuItem.icon"
+        :description="menuItem.description"
+        :state="menuItem.state"
+      />
     </span>
   </nav>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'SidebarMenu',
-
-  data() {
-    return {
-      menuItems: [
-        {
-          link: '#home',
-          icon: 'H',
-          description: 'Home',
-        },
-        {
-          link: '#training',
-          icon: 'T',
-          description: 'Training',
-        },
-        {
-          link: '#profile',
-          icon: 'P',
-          description: 'Profil',
-        },
-        {
-          link: '#stats',
-          icon: 'S',
-          description: 'Lernstatistik',
-        },
-        {
-          link: '#tutorial',
-          icon: 'T',
-          description: 'Tutorial',
-        },
-      ],
-      show: false,
-    }
-  },
-})
-</script>
-
-<style scoped>
-nav {
-  position: fixed;
-  top: 0;
-  left: 0;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  justify-content: center;
-}
-
-li {
-  padding-left: 20px;
-  padding-bottom: 50px;
-  text-align: left;
-  list-style-type: none;
-}
-
-a {
-  color: black;
-  text-decoration: none;
-}
-
-.icon {
-  margin-right: 25px;
-}
-</style>
+<style scoped></style>
