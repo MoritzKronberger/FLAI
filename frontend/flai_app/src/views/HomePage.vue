ecff9c487bfae4d4ac560bae5fbcedfbccca6146
jelllea <lea.jell@hs-augsburg.de>
2021-12-19 11:41:14 +0100

    Create view home

---
+++ frontend/flai_app/src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>Hallo, {{ username }}!</h2>
+</template>
+
+<script lang="ts">
+import { inject, computed } from 'vue'
+
+export default {
+  setup() {
+    const store: any = inject('store')
+
+    const username = computed(() => store.userdata.user.username)
+  },
+}
+</script>
