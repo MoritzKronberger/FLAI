50d0b193fc724a4149c2b442cd59f64e6e9e6333
ekun <eva.kunzmann@hs-augsburg.de>
2021-12-19 13:14:50 +0100

    Add SidebarMenu to App for testing

--- frontend/flai_app/src/App.vue
+++ frontend/flai_app/src/App.vue
@@ -2,11 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import SidebarMenu from './components/SidebarMenu.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hallo, wir sind FLAI!" />
+  <SidebarMenu />
 </template>
 
 <style>
