04d9ab04cbaf13ce027c7f75e43f32847217f42c
jelllea <lea.jell@hs-augsburg.de>
2021-12-19 11:56:42 +0100

    Add logo and styling to app

--- frontend/flai_app/src/App.vue
+++ frontend/flai_app/src/App.vue
@@ -8,12 +8,12 @@ onMounted(store.signdata.methods.createNewSigns)
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img id="logo" alt="flai logo" src="./assets/flai_logo.jpg" />
   <div id="nav">
     <router-link to="/">Home</router-link>
     <router-link to="/store">ShowStore</router-link>
   </div>
-  <router-view />
+  <router-view id="view" />
 </template>
 
 <style>
@@ -21,8 +21,24 @@ onMounted(store.signdata.methods.createNewSigns)
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+a {
+  margin: 1%;
+  text-decoration: none;
+}
+#nav {
+  text-align: center;
+}
+#logo {
+  margin: 1%;
+  max-width: 15%;
+  left: 0;
+  top: 0;
+  position: absolute;
+}
+#view {
+  margin-left: 20%;
 }
 </style>
