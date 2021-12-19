7eca2e1283d97950f9ab4bbaaa566965a5c6aaf6
jelllea <lea.jell@hs-augsburg.de>
2021-12-19 12:42:01 +0100

    Add row styling

--- frontend/flai_app/src/views/HomePage.vue
+++ frontend/flai_app/src/views/HomePage.vue
@@ -2,6 +2,31 @@
   <h2>
     Hallo, <b>{{ user.username }}!</b>
   </h2>
+  <div class="row">
+    <div>
+      <h2>Tagesaufgabe</h2>
+      <img
+        id="exercisePicture"
+        alt="Tagesaufgabe"
+        src="../assets/exercise.png"
+      />
+    </div>
+    <div>
+      <h2>Abzeichen</h2>
+      <img
+        id="badge"
+        alt="Abzeichen des aktuellen Levels"
+        src="../assets/badgeLevel3.png"
+      />
+    </div>
+  </div>
+  <h2>Statistik</h2>
+  <div class="row">
+    <div class="statistic">
+      <p class="number">5</p>
+      <p class="underline">Statistik Platzhalter</p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -23,5 +48,15 @@ export default {
 <style>
 h2 {
   font-weight: 200;
+}
+.row {
+  display: flex;
+}
+.row img {
+  height: 30vh;
+}
+#exercisePicture {
+  margin: 0;
+  border-radius: 3vw;
 }
 </style>
