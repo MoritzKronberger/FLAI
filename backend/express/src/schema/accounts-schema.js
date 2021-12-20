005009ae34798f5e42871689fba17aa96252c293
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-20 12:21:03 +0100

    Update accounts-schema

--- backend/express/src/schema/accounts-schema.js
+++ backend/express/src/schema/accounts-schema.js
@@ -1,10 +1,10 @@
 import Joi from 'joi'
 
 const createAccount = Joi.object({
-  username: Joi.string().min(5).max(40).required(),
+  username: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{5,30}$')).required(),
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
   repeatPassword: Joi.ref('password'),
-  email: Joi.string().email(),
+  email: Joi.string().email().required(),
 })
 
 export default createAccount
