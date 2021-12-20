bf2daaddf3a4b8061becfd63408b0007442d1905
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-20 12:21:30 +0100

    Create auth Token Function

--- backend/express/src/util/auth.js
+++ backend/express/src/util/auth.js
@@ -1,1 +1,18 @@
-// auth with webtokens
+import jwt from 'jsonwebtoken'
+
+function authToken(req, res, next) {
+  const authHeader = req.headers['authorization'][1]
+  console.log(authHeader)
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token == null) return res.sendStatus(401)
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403) // unvalid token
+    req.user = user
+    next()
+  })
+}
+
+export { authToken }
+export default { authToken }
