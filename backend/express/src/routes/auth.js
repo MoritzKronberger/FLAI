04085ef1399f2be9edccab68c0e03e523ae6089e
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-20 12:20:11 +0100

    Create auth post

--- backend/express/src/routes/auth.js
+++ backend/express/src/routes/auth.js
@@ -1,1 +1,16 @@
-// post login and register
+import dotenv from 'dotenv'
+import express from 'express'
+import jwt from 'jsonwebtoken'
+const auth = express.Router()
+
+dotenv.config()
+
+auth.post('/', (req, res) => {
+  const username = req.body.username
+  const user = { name: username }
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+  res.json({ accessToken: accessToken })
+})
+
+export { auth }
+export default { auth }
