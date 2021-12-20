18c168a20b28b5fd7ec7cb41b2ef703527f355ea
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-20 17:15:54 +0100

    Add function to generate expiring access_token

--- backend/express/src/util/auth.js
+++ backend/express/src/util/auth.js
@@ -5,12 +5,15 @@ function authToken(req, res, next) {
   const token = req.headers[authHeader]
   if (token == null) return res.sendStatus(401)
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    console.log(err)
     if (err) return res.sendStatus(403) // unvalid token
     req.user = user
     next()
   })
 }
 
-export { authToken }
-export default { authToken }
+function generateAccessToken(user, ACCESS_TOKEN_SECRET) {
+  return jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
+}
+
+export { authToken, generateAccessToken }
+export default { authToken, generateAccessToken }
