cfae566c774a7a39619b414ae312a31494e46631
Stefanie <stefanie.jany@hs-augsburg.de>
2021-12-21 08:29:50 +0100

    grid testing

--- frontend/flai_app/src/components/HelloWorld.vue
+++ frontend/flai_app/src/components/HelloWorld.vue
@@ -31,6 +31,18 @@ const count = ref(0)
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
+
+  <div class="grid-container">
+    <div class="grid-item">1</div>
+    <div class="grid-item">2</div>
+    <div class="grid-item">3</div>
+    <div class="grid-item">4</div>
+    <div class="grid-item">5</div>
+    <div class="grid-item">6</div>
+    <div class="grid-item">7</div>
+    <div class="grid-item">8</div>
+    <div class="grid-item">9</div>
+  </div>
 </template>
 
 <style lang="scss">
