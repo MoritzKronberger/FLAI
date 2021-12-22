d5dfb40092aa2edbc8acbbf992d2c05648614c0b
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-12-22 11:17:35 +0100

    Update get_full_sign to fit frontend store structure

--- backend/postgres_db/sql/11_sign_views.sql
+++ backend/postgres_db/sql/11_sign_views.sql
@@ -15,15 +15,11 @@ SELECT s."id", s."name", mc."name" As "motion_category"
 FROM "sign" s
      JOIN "e_motion_category" mc ON s."motion_category_id" = mc."id";
 
-CREATE VIEW get_full_sign ("id", "name", "motion_category", "recording")
+CREATE VIEW get_full_sign ("id", "name", "motion_category", "recordings")
 AS
-SELECT s."id", s."name", s."motion_category", JSONB_AGG(JSONB_BUILD_OBJECT('path', sr."path", 
-                                                                           'mimetype', mt."name",
-                                                                           'perspective', p."name")) "recording"
+SELECT s."id", s."name", s."motion_category", ARRAY_AGG(sr."id") "recordings"
 FROM get_sign s
      JOIN "sign_recording" sr ON s."id" = sr."sign_id"
-     JOIN "e_mimetype" mt     ON sr."mimetype_id" = mt."id"
-     JOIN "e_perspective" p   ON sr."perspective_id" = p."id"
 GROUP BY s."id", s."name", s."motion_category";
 
 COMMIT;
