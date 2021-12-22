cff3872985ebb12d97ab63413a6f9ab9d98adb74
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-12-22 11:43:45 +0100

    Add get_full_exercise_for_user view

--- backend/postgres_db/sql/13_exercise_views.sql
+++ backend/postgres_db/sql/13_exercise_views.sql
@@ -5,8 +5,9 @@
 BEGIN;
 
 /* Cleanup */
-DROP VIEW IF EXISTS get_exercise      CASCADE;
-DROP VIEW IF EXISTS get_full_exercise CASCADE;
+DROP VIEW IF EXISTS get_exercise               CASCADE;
+DROP VIEW IF EXISTS get_full_exercise          CASCADE;
+DROP VIEW IF EXISTS get_full_exercise_for_user CASCADE;
 
 /* Views */
 CREATE VIEW get_exercise ("id", "name", "description")
@@ -23,5 +24,16 @@ FROM get_exercise e
      JOIN "exercise_settings" es ON e."id" = es."exercise_id"
      JOIN "task" t               ON e."id" = t."exercise_id"
 GROUP BY e."id", e."name", e."description", es."level_1", es."level_2", es."level_3", es."sort_signs_by_order";
+
+CREATE VIEW get_full_exercise_for_user ("id", "user_id", "name", "description", 
+                                        "level_1", "level_2", "level_3", "sort_signs_by_order", 
+                                        "task_split", "word_length", "unlocked_signs",
+                                        "tasks")
+AS SELECT fe."id", esu."user_id", fe."name", fe."description", 
+          fe."level_1", fe."level_2", fe."level_3", fe."sort_signs_by_order", 
+          esu."task_split", esu."word_length", esu."unlocked_signs",
+          fe."tasks"
+FROM get_full_exercise fe
+     JOIN "exercise_settings_user" esu ON fe."id" = esu."exercise_id";
 
 COMMIT;
