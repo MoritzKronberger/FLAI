3e05ac2db1d895c4fb38d18c5bb2b4e274d13abe
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-23 12:59:20 +0100

    Add post, patch and deleteUser

--- backend/express/src/db/user.js
+++ backend/express/src/db/user.js
@@ -12,5 +12,41 @@ const getUser = async (id) => {
     : { status: 200, result: result.rows[0] }
 }
 
-export { getUser }
-export default { getUser }
+const postUser = async (data) => {
+  const result = await query(
+    `SELECT result
+    FROM pg_axios
+         ('user', $1,'POST'
+         );`,
+    [data]
+  )
+  return result.rows[0]
+}
+
+const patchUser = async (id, data) => {
+  const result = await query(
+    `SELECT result FROM pg_axios
+    ('user',
+      $2,
+     'PATCH',
+     $1);`,
+    [{ id: id }, data]
+  )
+
+  return result.rows[0]
+}
+
+const deleteUser = async (id) => {
+  const result = await query(
+    `SELECT result FROM pg_axios
+    ('user',
+      NULL,
+     'DELETE',
+     $1);`,
+    [{ id: id }]
+  )
+  return result.rows[0]
+}
+
+export { getUser, postUser, patchUser, deleteUser }
+export default { getUser, postUser, patchUser, deleteUser }
