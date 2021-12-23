cc604291d7b0db53ce54beac2ccbeff1893a0a35
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-23 18:00:08 +0100

    Add exerciseSettings, signRecordings and task to routes

--- backend/express/src/server.js
+++ backend/express/src/server.js
@@ -6,16 +6,22 @@ import cors from 'cors'
 dotenv.config()
 //import routes
 import { exercise } from './routes/exercise.js'
+import { exerciseSettings } from './routes/exercise_settings.js'
 import { signs } from './routes/signs.js'
+import { signRecording } from './routes/sign_recording.js'
 import { user } from './routes/user.js'
+import { task } from './routes/task.js'
 import { auth } from './routes/auth.js'
 
 // middleware
 app.use(express.json())
 app.use(cors())
-app.use('/api/exercises', exercise)
-app.use('/api/signs', signs)
+app.use('/api/exercise', exercise)
+app.use('/api/exercise-settings', exerciseSettings)
+app.use('/api/sign', signs)
+app.use('/api/sign-recording', signRecording)
 app.use('/api/user', user)
+app.use('/api/task', task)
 app.use('/api', auth)
 
 // server
