efb4132ad92c0cfc1ebf365e9d1c59b77e2b44c6
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-23 13:01:32 +0100

    Add db placeholder for login

--- backend/express/src/routes/auth.js
+++ backend/express/src/routes/auth.js
@@ -8,6 +8,7 @@ let refreshTokens = []
 auth.post('/login', (req, res) => {
   const username = req.body.username
   const user = { name: username }
+  // DB Querie
   const accessToken = generateAccessToken(user)
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
   refreshTokens.push(refreshToken)
