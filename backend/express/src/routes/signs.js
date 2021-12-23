9cd70a857d100132620b5f7b4ee7d8a333b1dfc0
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-23 18:05:36 +0100

    Update sign GET route

--- backend/express/src/routes/signs.js
+++ backend/express/src/routes/signs.js
@@ -1,24 +1,15 @@
 import express from 'express'
+import { request } from './request.js'
 const signs = express.Router()
 
-// get all signs
-signs.get('/', async (req, res) => {
-  try {
-    const response = signs
-    res.json(response)
-  } catch (err) {
-    console.log(err.message)
-  }
-})
-// get single signs
 signs.get('/:id', async (req, res) => {
-  try {
-    const id = req.params.id
-    const response = signs[id]
-    res.json(response)
-  } catch (err) {
-    console.log(err.message)
-  }
+  await request({
+    method: 'GET',
+    table: 'get_full_sign',
+    ids: req.params.id,
+    select: ['id', 'name', 'motion_category', 'recordings'],
+    res: res,
+  })
 })
 
 export { signs }
