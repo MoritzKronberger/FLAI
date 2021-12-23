be6e17337c584578cd8d4935b3c3698531d892c7
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-23 12:58:53 +0100

    Add loginUser query

--- backend/express/src/db/auth.js
+++ backend/express/src/db/auth.js
@@ -1,1 +1,18 @@
-// account queries
+import { query } from '../db.js'
+
+const loginUser = async (email, password) => {
+  const result = await query(
+    `SELECT check_password($1::VARCHAR, $2::VARCHAR) AS authorized, 
+    "id"
+FROM  "user"
+WHERE "email" = $1::VARCHAR
+`,
+    [email, password]
+  ).rows[0]
+  return result
+    ? { status: result.authorized ? 200 : 401, id: result.id }
+    : { status: 401, id: '' }
+}
+
+export { loginUser }
+export default { loginUser }
