f634600ee1644cd6286e595f12b571e633dd736d
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-27 12:48:42 +0100

    Add exerciseSession and progress route

--- backend/express/src/server.js
+++ backend/express/src/server.js
@@ -7,21 +7,25 @@ dotenv.config()
 //import routes
 import { exercise } from './routes/exercise.js'
 import { exerciseSettings } from './routes/exercise_settings.js'
+import { exerciseSession } from './routes/exercise_session.js'
 import { signs } from './routes/signs.js'
 import { signRecording } from './routes/sign_recording.js'
 import { user } from './routes/user.js'
 import { task } from './routes/task.js'
 import { auth } from './routes/auth.js'
+import { progress } from './routes/progress.js'
 
 // middleware
 app.use(express.json())
 app.use(cors())
 app.use('/api/exercise', exercise)
 app.use('/api/exercise-settings', exerciseSettings)
+app.use('/api/exercise-session', exerciseSession)
 app.use('/api/sign', signs)
 app.use('/api/sign-recording', signRecording)
 app.use('/api/user', user)
 app.use('/api/task', task)
+app.use('/api/progress', progress)
 app.use('/api', auth)
 
 // server
