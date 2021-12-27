517e4f4b36c434c3fb2e8f1dc6eb5876fb1ceb57
jelllea <lea.jell@hs-augsburg.de>
2021-12-27 13:54:14 +0100

    Create component for the feedback exercise

---
+++ frontend/flai_app/src/components/FeedbackExercise.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>Feedback Learning Exercise</h1>
+  {{ word }}
+  <span v-for="letter in word" :key="letter.name">{{ letter.name }}</span>
+  <video src="source" type="video/webm" />
+  <p>TODO: Add webcam component</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from 'vue'
+
+export default defineComponent({
+  name: 'FeedbackExercise',
+  setup() {
+    const store: any = inject('store')
+    const exercises = store.exercisedata.exercises
+    const word = exercises[exercises.length - 1].signs
+
+    return { word }
+  },
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
