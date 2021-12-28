7bcf7fba523d98be93f3f58e181c3ed4b5c023a3
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-28 16:52:20 +0100

    Update axios

--- frontend/flai_app/src/common/service/rest.js
+++ frontend/flai_app/src/common/service/rest.js
@@ -1,16 +1,34 @@
 import axios from 'axios'
 
-const apiCall = axios.create({
-  baseURL: 'localhost:5000/',
-  withCredentials: false,
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-})
+//const AUTH_TOKEN = ''
+//axios.defaults.headers.common['Authorization'] = AUTH_TOKEN
+
+axios.defaults.baseURL = 'https://localhost:5000/'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+const config = (_method, _url, _data = null) => {
+  return {
+    method: _method,
+    url: _url,
+    data: _data,
+  }
+}
+
+const jsonResult = async (method, url, data = null) => {
+  try {
+    const res = await axios(config(method, url, data))
+    return {
+      status: res.status,
+      headers: res.headers,
+      data: res.data,
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 export default {
-  getApi(path) {
-    return apiCall.get(path)
+  async JsonAction(method, url, data = null) {
+    return await jsonResult(method, url, data)
   },
 }
