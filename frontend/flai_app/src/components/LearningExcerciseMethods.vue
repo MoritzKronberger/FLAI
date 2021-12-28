9b470dfc306f0cffc86df218a6d377a7ae0852d1
Kieu Pham <kieu.pham@hs-augsburg.de>
2021-12-28 12:23:23 +0100

    learning exercise video control

---
+++ frontend/flai_app/src/components/LearningExcerciseMethods.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <video ref="videoPlayer">
+      <source
+        src="../assets/signs/vid/front/a_vid_front.webm"
+        type="video/mp4"
+      />
+    </video>
+    <div>
+      <button @click="play">play</button>
+      <button @click="pause">pause</button>
+      <button @click="setSpeed(0.5)">0.5x</button>
+      <button @click="setSpeed(1)">1x</button>
+      <button @click="setSpeed(1.5)">1.5x</button>
+      <button @click="setSpeed(2)">2x</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LearningExerciseMethods',
+
+  setup() {
+    let isPlaying: true
+
+    function play() {
+      this.$refs.videoPlayer.play()
+    }
+
+    function pause() {
+      this.$refs.videoPlayer.pause()
+    }
+
+    function setSpeed(speed: number) {
+      this.$refs.videoPlayer.playbackRate = speed
+    }
+    return { play, pause, setSpeed }
+  },
+})
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
