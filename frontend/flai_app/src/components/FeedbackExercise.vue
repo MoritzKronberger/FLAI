9b9dc7e381abb4dd77d1b2dc7304d7ddc20e5e8e
jelllea <lea.jell@hs-augsburg.de>
2021-12-29 18:00:13 +0100

    Add method to get video source

--- frontend/flai_app/src/components/FeedbackExercise.vue
+++ frontend/flai_app/src/components/FeedbackExercise.vue
@@ -1,22 +1,43 @@
 <template>
   <h1>Feedback Learning Exercise</h1>
-  word: {{ word }}
-  <span v-for="letter in word" :key="letter.name">{{ letter.name }}</span>
-  <video src="source" type="video/webm" />
+  <p>
+    word:
+    <span v-for="letter in signs" :key="letter.name">{{ letter.name }}</span>
+  </p>
+  <video :src="videoSource" type="video/webm" autoplay loop />
+  <Vbutton
+    label="Switch Perspective"
+    btnclass="controls"
+    @click="switchPerspective()"
+  />
   <p>TODO: Add webcam component</p>
 </template>
 
 <script setup lang="ts">
-import { inject, reactive, computed, onBeforeMount, ComputedRef } from 'vue'
+import { inject, ref, computed, onBeforeMount, ComputedRef } from 'vue'
 import { Sign } from '../store/signdata'
 
 const store: any = inject('store')
-const word: ComputedRef<Sign[]> = computed(
+const signs: ComputedRef<Sign[]> = computed(
   () => store.exercisedata.exercises.at(-1).signs
 )
+const index = ref(0)
+const perspective = ref('front')
+
+function getSource() {
+  const rec = signs.value[0].recordings.find(
+    (el) => el.perspectiveId === perspective.value
+  )
+  console.log('el', JSON.stringify(rec))
+  if (rec === undefined) {
+    return 'src/assets/error.webm'
+  }
+  return rec.video
+}
+const videoSource: ComputedRef<string> = computed(() => getSource())
+
 onBeforeMount(() => {
   store.exercisedata.methods.startNewExercise('name', 'desc')
-  console.log('word', JSON.stringify(word))
 })
 </script>
 
@@ -25,15 +46,10 @@ onBeforeMount(() => {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+video {
+  width: 40%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.controls {
+  background: lightblue;
 }
 </style>
