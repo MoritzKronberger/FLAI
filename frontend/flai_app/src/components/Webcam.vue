945a1ae9dbf7984ae2ae5c857ea11133b80223d5
ekun <eva.kunzmann@hs-augsburg.de>
2021-12-29 15:17:18 +0100

    Change script syntax to script setup

--- frontend/flai_app/src/components/Webcam.vue
+++ frontend/flai_app/src/components/Webcam.vue
@@ -1,42 +1,34 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const webcamFeed = ref<HTMLVideoElement>()
+const stream = ref<MediaStream>()
+const constraints = {
+  video: {
+    facingMode: 'user',
+  },
+  audio: false,
+}
+const start = async (): Promise<void> => {
+  stream.value = await navigator.mediaDevices.getUserMedia(constraints)
+  if (!webcamFeed.value) throw new Error('Video reference is null')
+  webcamFeed.value.srcObject = stream.value
+}
+onMounted(() => {
+  if (!navigator.mediaDevices) {
+    window.alert(
+      'Es steht keine Webcam zur Verfügung. Bitte schließen Sie ein Gerät an und versuchen Sie es erneut.'
+    )
+    throw new Error('Webcam is not available')
+  } else {
+    start()
+  }
+})
+</script>
+
 <template>
   <video id="webcam-feed" ref="webcamFeed" autoplay="true"></video>
 </template>
-
-<script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue'
-
-export default defineComponent({
-  name: 'WebCam',
-  setup() {
-    const webcamFeed = ref<HTMLVideoElement>()
-    const stream = ref<MediaStream>()
-    const constraints = {
-      video: {
-        facingMode: 'user',
-      },
-      audio: false,
-    }
-    const start = async (): Promise<void> => {
-      stream.value = await navigator.mediaDevices.getUserMedia(constraints)
-      if (!webcamFeed.value) throw new Error('Video reference is null')
-      webcamFeed.value.srcObject = stream.value
-    }
-    onMounted(() => {
-      if (!navigator.mediaDevices) {
-        window.alert(
-          'Es steht keine Webcam zur Verfügung. Bitte schließen Sie ein Gerät an und versuchen Sie es erneut.'
-        )
-        throw new Error('Webcam is not available')
-      } else {
-        start()
-      }
-    })
-    return {
-      webcamFeed,
-    }
-  },
-})
-</script>
 
 <style lang="css" scoped>
 #webcam-feed {
