53da3159f3ac19f70672530f11d6823278e1bb5a
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-30 16:24:58 +0100

    Add getTask action

--- frontend/flai_app/src/store/exercisedata.ts
+++ frontend/flai_app/src/store/exercisedata.ts
@@ -71,6 +71,7 @@ const methods = {
 }
 
 const actions = {
+  /* eslint-disable */
   async getAllExercises() {
     jsonAction({ method: 'get', url: 'exercise/all', data: {} })
   },
@@ -81,15 +82,26 @@ const actions = {
       url: 'exercise',
       data: {
         id: '81cb9652-c202-4675-a55d-81296b7d17b6',
-        user_id: '079c8725-3b47-434c-ba1a-afe3a8162dac', // eslint-disable-line
+        user_id: '079c8725-3b47-434c-ba1a-afe3a8162dac',
       },
     })
   },
+  async getTask() {
+    jsonAction({
+      method: 'get',
+      url: 'task',
+      data: {
+        exercise_id: '81cb9652-c202-4675-a55d-81296b7d17b6',
+      },
+    })
+  },
+  /* eslint-enable */
 }
 
 const exerciseData = {
   exerciseSettings: readonly(exerciseSettings) as ExerciseSettings,
   exercises: readonly(exercises) as Exercise[],
   methods,
+  actions,
 }
-export default { exerciseData, actions }
+export default exerciseData
