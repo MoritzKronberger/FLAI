a76ebd1c677441c74be7ad248b5d68a6c32d328a
jelllea <lea.jell@hs-augsburg.de>
2021-12-30 17:04:42 +0100

    Add video and method to update progress

--- frontend/flai_app/src/components/FeedbackExercise/ShowWord.vue
+++ frontend/flai_app/src/components/FeedbackExercise/ShowWord.vue
@@ -1,10 +1,38 @@
 <template>
-  <p>{{ props.signs }}</p>
+  <div vFocus tabindex="0" @keydown.c="correct">
+    <div vFocus tabindex="0" @keydown.w="wrong">
+      <p v-for="letter in props.signs" :key="letter.name">{{ letter.name }}</p>
+      <Video :signs="signs" :index="index" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { inject, ref } from 'vue'
+import router from '../../router'
 import { Sign } from '../../store/signdata'
+import Video from './Video.vue'
+
+const store: any = inject('store')
+
+const index = ref(0)
 
 const props = defineProps<{ signs: Sign[] }>()
+
+function correct() {
+  store.signdata.methods.updateProgress(props.signs[index.value].name, 10)
+  if (index.value < props.signs.length - 1) {
+    index.value++
+  } else {
+    router.push('HomePage')
+  }
+}
+function wrong() {
+  store.signdata.methods.updateProgress(props.signs[index.value].name, -10)
+  if (index.value < props.signs.length - 1) {
+    index.value++
+  } else {
+    router.push('HomePage')
+  }
+}
 </script>
