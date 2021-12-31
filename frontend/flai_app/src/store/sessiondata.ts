7bc7316639ad7426d0b14746ef71eb25320c1b2d
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-31 11:16:24 +0100

    Remove exercise session routes from sessiondata

--- frontend/flai_app/src/store/sessiondata.ts
+++ frontend/flai_app/src/store/sessiondata.ts
@@ -27,63 +27,9 @@ const methods = {
   },
 }
 
-const actions = {
-  /* eslint-disable */
-  async getActiveExerciseSession() {
-    jsonAction({
-      method: 'get',
-      url: 'exercise-session',
-      data: {
-        exercise_id: '81cb9652-c202-4675-a55d-81296b7d17b6',
-        user_id: '079c8725-3b47-434c-ba1a-afe3a8162dac',
-      },
-    })
-  },
-  async postNewExerciseSession() {
-    jsonAction({
-      method: 'post',
-      url: 'exercise-session',
-      data: {
-        exercise_id: '81cb9652-c202-4675-a55d-81296b7d17b6',
-        user_id: '7600c936-7c07-4e4d-98ec-243612652ca3',
-        start_time: '2021-12-27 13:12:00.595133+00',
-      },
-    })
-  },
-  async patchExerciseSession() {
-    jsonAction({
-      method: 'patch',
-      url: 'exercise-session',
-      data: {
-        data: {
-          session_duration: '00:50:00',
-        },
-        ids: {
-          exercise_id: '81cb9652-c202-4675-a55d-81296b7d17b6',
-          user_id: '7600c936-7c07-4e4d-98ec-243612652ca3',
-          start_time: '2021-12-27 13:11:00.595133+00',
-        },
-      },
-    })
-  },
-  async deleteExerciseSession() {
-    jsonAction({
-      method: 'delete',
-      url: 'exercise-session',
-      data: {
-        exercise_id: '81cb9652-c202-4675-a55d-81296b7d17b6',
-        user_id: '7600c936-7c07-4e4d-98ec-243612652ca3',
-        start_time: '2021-12-27 13:12:00.595133+00',
-      },
-    })
-  },
-  /* eslint-enable */
-}
-
 const sessiondata = {
   session: readonly(session) as Session,
   methods,
-  actions,
 }
 
 export default sessiondata
