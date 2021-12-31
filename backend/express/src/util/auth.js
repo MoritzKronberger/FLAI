341a4c32b740098ab95e9c6d5763a95083f09e1d
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-31 17:03:06 +0100

    Update authToken to use id instead of name

--- backend/express/src/util/auth.js
+++ backend/express/src/util/auth.js
@@ -3,15 +3,15 @@ function authToken(req, res, next) {
   const authHeader = req.headers['authorization']
   const token = req.headers[authHeader]
   if (token == null) return res.sendStatus(401)
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, id) => {
     if (err) return res.sendStatus(403) // unvalid token
-    req.user = user
+    req.id = id
     next()
   })
 }
 
-function generateAccessToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '40s' })
+function generateAccessToken(id) {
+  return jwt.sign(id, process.env.ACCESS_TOKEN_SECRET)
 }
 
 export { authToken, generateAccessToken }
