200a22ceba2d4b9bafb94fc82286c5532e876817
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-12-31 10:55:51 +0100

    Move handpose options to store

--- frontend/flai_app/src/components/Handpose.vue
+++ frontend/flai_app/src/components/Handpose.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import { onMounted, Ref, ref } from 'vue'
+import { computed, inject, onMounted, Ref, ref } from 'vue'
 import webcam from '../components/Webcam.vue'
 import { Hands, Results } from '@mediapipe/hands'
 import { Camera } from '@mediapipe/camera_utils'
 
+const store: any = inject('store')
+
 const handposeReady = ref(false)
 let hands: Hands
+const handposeOptions = computed(() => store.handposedata.handposeOptions)
 
 const emit = defineEmits(['newResult', 'statusChange'])
 const onResults = (results: Results) => {
@@ -21,12 +24,7 @@ const loadMediapipeHands = (): void => {
       return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
     },
   })
-  hands.setOptions({
-    maxNumHands: 1,
-    modelComplexity: 1,
-    minDetectionConfidence: 0.8,
-    minTrackingConfidence: 0.8,
-  })
+  hands.setOptions(handposeOptions.value)
   hands.onResults(onResults)
 }
 
--- frontend/flai_app/src/store/index.ts
+++ frontend/flai_app/src/store/index.ts
@@ -2,10 +2,12 @@ import userdata from './userdata'
 import sessiondata from './sessiondata'
 import exercisedata from './exercisedata'
 import signdata from './signdata'
+import handposedata from './handposedata'
 
 export default {
   userdata,
   sessiondata,
   exercisedata,
   signdata,
+  handposedata,
 }
