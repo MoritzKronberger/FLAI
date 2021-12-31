a483e46a50407a1905aa0c524dd8fc8b641889c8
jelllea <lea.jell@hs-augsburg.de>
2021-12-31 15:59:11 +0100

    Delete DropDownMenu from App

--- frontend/flai_app/src/App.vue
+++ frontend/flai_app/src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { provide, onMounted, ref } from 'vue'
 import store from './store'
-import { DropDown } from './ressources/ts/interfaces'
-import LearningExcerciseMethods from './components/LearningExcerciseMethods.vue'
 import DropDownMenu from './components/DropDownMenu.vue'
 
 provide('store', store)
@@ -10,16 +8,6 @@ provide('store', store)
 onMounted(store.exercisedata.methods.getExercises) // fake frontend method
 onMounted(store.exercisedata.actions.getAllExercises) // real backend action
 onMounted(store.sessiondata.methods.startTimer)
-
-const dropDownItems: DropDown[] = [
-  { label: '1x', value: 1 },
-  { label: '0.5x', value: 0.5 },
-  { label: '0.25x', value: 0.25 },
-]
-
-function changeSpeed(output: any) {
-  console.log(output)
-}
 
 function handleInput(e: Event) {
   const target = <HTMLInputElement>e.target
@@ -38,11 +26,6 @@ function handleInput(e: Event) {
   <main>
     <router-view />
   </main>
-  <DropDownMenu
-    title="Geschwindigkeit"
-    :items="dropDownItems"
-    @click-element="changeSpeed"
-  />
 </template>
 
 <style>
