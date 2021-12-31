2ac0d81fd151090901f4cc050fb6d03673ebeb2c
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-12-31 11:15:53 +0100

    Add patchExerciseSettings and session related route

--- frontend/flai_app/src/store/exercisedata.ts
+++ frontend/flai_app/src/store/exercisedata.ts
@@ -80,18 +80,85 @@ const actions = {
     jsonAction({
       method: 'get',
       url: 'exercise',
+      // id == exercise_id
       data: {
         id: '81cb9652-c202-4675-a55d-81296b7d17b6',
         user_id: '079c8725-3b47-434c-ba1a-afe3a8162dac',
       },
     })
   },
+
+  async patchExerciseSettings() {
+    jsonAction({
+      method: 'patch',
+      url: 'exercise-settings',
+      data: {
+        ids: {
+          exercise_id: '81cb9652-c202-4675-a55d-81296b7d17b6',
+          user_id: '079c8725-3b47-434c-ba1a-afe3a8162dac',
+        },
+        data: {
+          task_split: 0.7,
+          word_length: 5,
+        },
+      },
+    })
+  },
+
   async getTask() {
     jsonAction({
       method: 'get',
       url: 'task',
       data: {
         exercise_id: '81cb9652-c202-4675-a55d-81296b7d17b6',
+      },
+    })
+  },
+  async getActiveExerciseSession() {
+    jsonAction({
+      method: 'get',
+      url: 'exercise-session',
+      data: {
+        exercise_id: '81cb9652-c202-4675-a55d-81296b7d17b6',
+        user_id: '079c8725-3b47-434c-ba1a-afe3a8162dac',
+      },
+    })
+  },
+  async postNewExerciseSession() {
+    jsonAction({
+      method: 'post',
+      url: 'exercise-session',
+      data: {
+        exercise_id: '81cb9652-c202-4675-a55d-81296b7d17b6',
+        user_id: '7600c936-7c07-4e4d-98ec-243612652ca3',
+        start_time: '2021-12-31 13:12:00.595133+00',
+      },
+    })
+  },
+  async patchExerciseSession() {
+    jsonAction({
+      method: 'patch',
+      url: 'exercise-session',
+      data: {
+        data: {
+          session_duration: '00:50:00',
+        },
+        ids: {
+          exercise_id: '81cb9652-c202-4675-a55d-81296b7d17b6',
+          user_id: '7600c936-7c07-4e4d-98ec-243612652ca3',
+          start_time: '2021-12-31 13:12:00.595133+00',
+        },
+      },
+    })
+  },
+  async deleteExerciseSession() {
+    jsonAction({
+      method: 'delete',
+      url: 'exercise-session',
+      data: {
+        exercise_id: '81cb9652-c202-4675-a55d-81296b7d17b6',
+        user_id: '7600c936-7c07-4e4d-98ec-243612652ca3',
+        start_time: '2021-12-31 13:12:00.595133+00',
       },
     })
   },
