79b5338eca80a2b04a8938442aaedc1464169fcb
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-01 17:51:00 +0100

    Add buffer controls

--- frontend/flai_app/src/views/FlaiNetTest.vue
+++ frontend/flai_app/src/views/FlaiNetTest.vue
@@ -1,23 +1,32 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, inject, ref } from 'vue'
 import { FlaiNetResults } from '../store/flainetdata'
 import flaiNet from '../components/FlaiNet.vue'
+import customCheckbox from '../components/CustomCheckbox.vue'
+import textInputField from '../components/TextInputField.vue'
+
+const store: any = inject('store')
+const flaiNetMethods = computed(() => store.flainetdata.methods)
+const bufferedResult = ref(true)
+const bufferSize = ref('15')
+
+const setBufferedResult = (): void => {
+  flaiNetMethods.value.changeResultBuffer(bufferedResult.value)
+}
+const setBufferSize = (): void => {
+  flaiNetMethods.value.changeResultBufferSize(parseInt(bufferSize.value))
+}
 
 const flaiNetResults = ref<FlaiNetResults>()
 const flaiNetReady = ref(false)
 
 const setFlaiNetResults = (result: FlaiNetResults): void => {
   flaiNetResults.value = result
+  console.log(result)
 }
 const setflaiNetReady = (result: boolean): void => {
   flaiNetReady.value = result
 }
-/*       flaiNetResults?.[0]
-        ? flaiNetResults?.[0].inconclusive
-          ? 'Detecting ... please hold.'
-          : `${flaiNetResults[0].label} [${flaiNetResults[0].confidence}]`
-        : 'No hand detected.'*/
-//(!flaiNetResults[0].uniformLabels ? 'Detecting... Please hold.' : `${flaiNetResults[0].label} [${flaiNetResults[0].confidence}]`)
 </script>
 
 <template>
@@ -29,10 +38,31 @@ const setflaiNetReady = (result: boolean): void => {
     Prediction:
     {{
       flaiNetResults?.[0]
-        ? !flaiNetResults[0].uniformLabels
+        ? !flaiNetResults[0].uniformLabels && bufferedResult
           ? 'Detecting... Please hold.'
           : `${flaiNetResults[0].label} [${flaiNetResults[0].confidence}]`
         : 'No hand detected.'
     }}
+  </div>
+  <br />
+  <br />
+  <br />
+  <div>
+    <custom-checkbox
+      v-model="bufferedResult"
+      label-name="Use Result Buffer"
+      element-class="checkbox-primary"
+      @input="setBufferedResult"
+    />
+  </div>
+  <br />
+  <div>
+    <text-input-field
+      v-model="bufferSize"
+      label-name="Buffer Size"
+      placeholder="buffersize"
+      element-class="input-primary"
+      @keyup.enter="setBufferSize"
+    />
   </div>
 </template>
