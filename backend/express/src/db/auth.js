69cb674987c344477f9a79a8e74e706e74e58255
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-03 11:42:45 +0100

    Update to fit new check_password db function

--- backend/express/src/db/auth.js
+++ backend/express/src/db/auth.js
@@ -1,17 +1,12 @@
 import { query } from '../db.js'
 
-const loginUser = async (email, password) => {
+const loginUser = async (loginData) => {
   const result = await query(
-    `SELECT check_password($1::D_EMAIL, $2::VARCHAR) AS authorized, 
-    "id"
-FROM  "user"
-WHERE "email" = $1::D_EMAIL
-`,
-    [email, password]
+    `SELECT result
+     FROM   check_password($1);`,
+    [loginData]
   )
   return result.rows[0]
-    ? { status: result.rows[0].authorized ? 200 : 401, id: result.rows[0].id }
-    : { status: 401, id: '' }
 }
 
 export { loginUser }
