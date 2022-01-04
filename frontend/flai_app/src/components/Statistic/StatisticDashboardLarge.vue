28766b56233b4c4863c58112aa864ab3eafb756c
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-04 13:06:09 +0100

    Create base components

---
+++ frontend/flai_app/src/components/Statistic/StatisticDashboardLarge.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import DayStreak from './DayStreak.vue'
+const days = 15
+</script>
+
+<template>
+  <DayStreak :days="days" />
+</template>
+
+<style scoped>
+/** here place for import scss */
+</style>
