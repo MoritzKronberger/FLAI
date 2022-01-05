457b4107f6f8ca960700459f2b556efc46a01a75
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-05 09:42:02 +0100

    Add logout button and implement FLAI icon via IconLoader

--- frontend/flai_app/src/App.vue
+++ frontend/flai_app/src/App.vue
@@ -1,38 +1,36 @@
 <script setup lang="ts">
-import { provide, onMounted, ref } from 'vue'
 import store from './store'
 import SidebarMenu from './components/Sidebar/SidebarMenu.vue'
+import IconLoader from './components/IconLoader.vue'
+import customButton from './components/CustomButton.vue'
+import { provide } from 'vue'
 
 provide('store', store)
 
-//onMounted(store.exercisedata.methods.getExercises) // fake frontend method
-//onMounted(store.exercisedata.actions.getAllExercises) // real backend action
-onMounted(store.sessiondata.methods.startTimer)
-
-function handleInput(e: Event) {
-  const target = <HTMLInputElement>e.target
-
-  console.log('Das ist der Input:', target.value)
+const logoutUser = (): void => {
+  console.log('Logout')
 }
 </script>
 
 <template>
-  <img id="logo" alt="flai logo" src="./assets/flai_logo.jpg" />
-  <div id="nav">
-    <router-link :to="{ name: 'HomePage' }">Home</router-link>
-    <router-link :to="{ name: 'ShowStore' }">ShowStore</router-link>
-    <router-link :to="{ name: 'LearningExercise' }">Exercise</router-link>
-    <router-link :to="{ name: 'TestComponents' }">TestComponents</router-link>
-    <router-link :to="{ name: 'TestFlaiNet' }">TestFlaiNet</router-link>
-    <router-link :to="{ name: 'ProfilePage' }">Profile</router-link>
-    <router-link :to="{ name: 'RegisterPage' }">Register</router-link>
-    <router-link :to="{ name: 'LoginPage' }">Login</router-link>
-  </div>
+  <header>
+    <IconLoader
+      path="../assets/flai_logo"
+      mimetype="jpg"
+      alt="FLAI Icon"
+      element-class="flai-header-icon"
+    />
+  </header>
   <main>
     <router-view />
   </main>
   <aside>
     <SidebarMenu />
+    <custom-button
+      label="Logout"
+      btnclass="button-primary"
+      @button-click="logoutUser"
+    />
   </aside>
 </template>
 
