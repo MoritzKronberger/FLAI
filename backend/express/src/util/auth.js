cb33a180406e8e38ee230c24f783df55950b949d
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-01-06 15:10:43 +0100

    Update authtoken to verify user_id

--- backend/express/src/util/auth.js
+++ backend/express/src/util/auth.js
@@ -1,9 +1,10 @@
 import jwt from 'jsonwebtoken'
-function authToken(req, res, next) {
+function authToken(userId, req, res, next) {
   const token = req.headers['authorization']
   if (token == null) return res.sendStatus(401)
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, id) => {
     if (err) return res.sendStatus(403) // unvalid token
+    if (id !== userId) return res.sendStatus(401)
     req.id = id
     next()
   })
