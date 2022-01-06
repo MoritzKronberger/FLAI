7bc2884a1c9bc2b8403f7c6492b34c94f3f9d7ed
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-06 14:16:56 +0100

    Add function to use jwt from sessionStorage to re-authenticate

--- frontend/flai_app/src/ressources/ts/methods.ts
+++ frontend/flai_app/src/ressources/ts/methods.ts
@@ -1,4 +1,29 @@
+import { computed } from 'vue'
+import store from '../../store'
+
 export function errorMessage(message?: string) {
   if (!message) message = 'Sorry, there was an error.'
   alert(message)
+}
+
+// TODO: load into localStorage once tokens have expiration time
+export async function authenticateFromSessionStorage() {
+  const isAuth = computed(() => store.authdata.auth.isAuth)
+
+  if (!isAuth.value) {
+    const jwt = sessionStorage.getItem('jsonWebToken')
+    const userId = sessionStorage.getItem('userId')
+    // TODO: test if jwt is valid for user id
+    if (jwt && userId) {
+      store.authdata.methods.saveAuthData({
+        token: jwt,
+        user_id: userId,
+        isAuth: true,
+      })
+      await store.authdata.actions.getApplicationData()
+    } else {
+      return false
+    }
+  }
+  return true
 }
