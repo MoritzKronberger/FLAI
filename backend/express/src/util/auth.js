431cbcfdd9376296ec6e5416e082c5a19fb2f922
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-07 09:54:09 +0100

    Add debug logs and todo

--- backend/express/src/util/auth.js
+++ backend/express/src/util/auth.js
@@ -1,11 +1,15 @@
 import jwt from 'jsonwebtoken'
 function authToken(req, res, next, userId) {
   const token = req.headers['authorization']
+  console.log('--- NEW REQUEST ---')
+  console.log(`Token: ${token}`)
   if (token == null) return res.sendStatus(401)
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, id) => {
+    console.log(`decoded-id: ${id}; user_id: ${userId}`)
     if (err) return res.sendStatus(403) // unvalid token
     if (id !== userId) return res.sendStatus(401)
-    req.id = id
+    // TODO: not needed?
+    //res.id = id
     next()
   })
 }
