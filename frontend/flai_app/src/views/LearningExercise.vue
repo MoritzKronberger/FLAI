7ec6713335ce595cd0f6963d044487fe8de0dec7
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-07 16:42:06 +0100

    Update to close camera on view exit

--- frontend/flai_app/src/views/LearningExercise.vue
+++ frontend/flai_app/src/views/LearningExercise.vue
@@ -1,9 +1,10 @@
 <template>
-  <FeedbackExercise />
+  <FeedbackExercise @handpose-started="setHandposeObjects" />
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { Camera } from '@mediapipe/camera_utils'
+import { computed, ref } from 'vue'
 import { onBeforeRouteLeave } from 'vue-router'
 import FeedbackExercise from '../components/FeedbackExercise/FeedbackExercise.vue'
 import store from '../store'
@@ -12,7 +13,14 @@ const session = computed(() => store.exercisedata.activeExerciseSession)
 
 const exerciseId = computed(() => store.exercisedata.exercises[0].id)
 
+const camera = ref<Camera>()
+
+const setHandposeObjects = (handposeCamera: Camera) => {
+  camera.value = handposeCamera
+}
+
 onBeforeRouteLeave(async () => {
+  if (camera.value) await camera.value.stop()
   await store.exercisedata.actions.patchExerciseSession(
     exerciseId.value,
     session.value,
