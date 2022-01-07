4103b6e2936788f48237b097f6ff1dcd96a94967
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-01-07 11:02:38 +0100

    Remove console log

--- backend/express/src/util/auth.js
+++ backend/express/src/util/auth.js
@@ -3,7 +3,6 @@ function authToken(req, res, next, userId) {
   const token = req.headers['authorization']
   if (token == null) return res.sendStatus(401)
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, id) => {
-    console.log(`decoded-id: ${id}; user_id: ${userId}`)
     if (err) return res.sendStatus(403) // unvalid token
     if (id !== userId) return res.sendStatus(401)
     next()
