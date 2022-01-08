b9ed8ca26bd2acead199ed31ca6562562dddb074
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-08 10:55:02 +0100

    Create get active_streak route

---
+++ backend/express/src/routes/statistic.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { request } from './request.js'
+//import { authToken } from '../util/auth.js'
+const statistic = express.Router()
+
+statistic.get('/active_streak', async (req, res) => {
+  await request({
+    method: 'GET',
+    table: 'get_active_streak',
+    selectCols: ['user_id', 'streak'],
+    ids: req.query,
+    res: res,
+  })
+})
+
+export { statistic }
+export default { statistic }
