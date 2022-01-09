a2365fe2c51494dcad7d6fe1abc5d1760500c3b3
Kieu Pham <kieu.pham@hs-augsburg.de>
2022-01-09 17:50:22 +0100

    add font scss

--- frontend/flai_app/src/views/TestFont.vue
+++ frontend/flai_app/src/views/TestFont.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Entdecke die neue Art des Lernens</h1>
+  <label>Label</label>
+  <br />
+  <p class="display-1">Display 1</p>
+  <p class="display-2">Display 2</p>
+  <br />
+  <h1>Heading 1</h1>
+  <h2>Heading 2</h2>
+  <h3>Heading 3</h3>
+  <h4>Heading 4</h4>
+  <br />
+  <body>
+    <p class="large">Body Large</p>
+    <p class="medium">Body Medium</p>
+    <p class="normal">Body Normal</p>
+    <p class="small">Body Small</p>
+    <p class="xsmall">Body Extra Small</p>
+  </body>
+  <br />
+  <p class="header-links">Header Links</p>
+  <p class="lp-text">Landing Page Text</p>
+  <p class="db-cal-date">Dashboard Calender Date</p>
 </template>
 
 <style></style>
