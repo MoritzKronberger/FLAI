58996560229fc54d998d1ed0acf6496893bd7faa
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-10 09:32:12 +0100

    Move TestFont to DebugPage

--- frontend/flai_app/src/views/HomePage.vue
+++ frontend/flai_app/src/views/HomePage.vue
@@ -6,7 +6,6 @@ import customButton from '../components/CustomButton.vue'
 import IconLoader from '../components/IconLoader.vue'
 import { useRouter } from 'vue-router'
 import store from '../store'
-import TestFont from './TestFont.vue'
 
 const router = useRouter()
 
@@ -19,8 +18,7 @@ const level = ref(1)
 
 <template>
   <div v-if="!auth.isAuth" class="landing-page">
-    <TestFont />
-    <!--<div>Wilkommen bei FLAI!</div>
+    <div>Wilkommen bei FLAI!</div>
     <div>
       Lerne mithilfe unserer AI die Grundlagen deutscher Gebärdensprache.
     </div>
@@ -31,7 +29,7 @@ const level = ref(1)
     />
     <div>
       Du hast bereits ein Konto?<router-link to="/login">Login</router-link>
-    </div>-->
+    </div>
   </div>
   <div v-if="auth.isAuth" class="home-page">
     <div class="dashboard">
