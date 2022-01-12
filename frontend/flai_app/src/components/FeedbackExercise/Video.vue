59e266b5b0ab37950d048bdb4ce375ac00661488
jelllea <lea.jell@hs-augsburg.de>
2022-01-12 18:22:00 +0100

    Make smaller video buttons

--- frontend/flai_app/src/components/FeedbackExercise/Video.vue
+++ frontend/flai_app/src/components/FeedbackExercise/Video.vue
@@ -11,12 +11,12 @@
       <div class="perspective-buttons">
         <CustomButton
           label="Front"
-          btnclass="sec_small_button_blue"
+          btnclass="video_controls_button_blue"
           @click="frontPerspective()"
         />
         <CustomButton
           label="Seite"
-          btnclass="sec_small_button_blue"
+          btnclass="video_controls_button_blue"
           @click="sidePerspective()"
         />
       </div>
@@ -25,7 +25,7 @@
           v-for="item in speedItems"
           :key="item.label"
           :label="item.label"
-          btnclass="sec_small_button_blue"
+          btnclass="video_controls_button_blue"
           @click="changeSpeed(item.value)"
         />
       </div>
