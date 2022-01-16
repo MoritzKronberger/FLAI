ed72efbffbc77454ee3818a3fa5074f0965a3397
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-16 10:51:08 +0100

    Update to fetch statistic values from store

--- frontend/flai_app/src/components/Statistic/StatisticDashboardSmall.vue
+++ frontend/flai_app/src/components/Statistic/StatisticDashboardSmall.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import store from '../../store'
 import CardSmall from './CardSmall.vue'
+import moment from 'moment'
+
+const timeLearnt = computed(
+  () => store.statisticdata.userStatistic.timeLearntToday
+)
+const currentExercise = computed(() => store.exercisedata.exercises[0].name)
+const bestSign = computed(
+  () => store.statisticdata.userStatistic.bestExerciseSign
+)
+const exerciseCompletion = computed(
+  () => store.statisticdata.userStatistic.exerciseCompletion
+)
 </script>
 
 <template>
@@ -8,23 +22,25 @@ import CardSmall from './CardSmall.vue'
     <div class="grid-cards">
       <div class="child-card">
         <card-small
-          statistic-value="42 Min"
+          :statistic-value="`${moment.duration(timeLearnt).asMinutes()} Min`"
           link-target="ComingSoon"
           statistic-text="heute gelernt"
         />
       </div>
       <div class="child-card">
         <card-small
-          statistic-value="E"
+          :statistic-value="bestSign ? bestSign.toUpperCase() : '-'"
           link-target="ComingSoon"
-          statistic-text="beste Gebärde aus Buchstabieren lernen"
+          :statistic-text="`beste Gebärde aus ${currentExercise}`"
         />
       </div>
       <div class="child-card">
         <card-small
-          statistic-value="40%"
+          :statistic-value="`${
+            exerciseCompletion ? Math.round(exerciseCompletion * 100) : 0
+          }%`"
           link-target="ComingSoon"
-          statistic-text="von Buchstabieren lernen abgeschlossen"
+          :statistic-text="`von ${currentExercise} abgeschlossen`"
         />
       </div>
     </div>
