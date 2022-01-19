56fc1e3c99a6b41e78a09c03e5cdcddff6bf236c
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-01-19 18:59:31 +0100

    Add Bar Chart

--- frontend/flai_app/src/components/Statistic/TrendDiagram.vue
+++ frontend/flai_app/src/components/Statistic/TrendDiagram.vue
@@ -1,18 +1,51 @@
 <script setup lang="ts">
-const date = new Date(Date.now())
+import { BarChart } from 'vue-chart-3'
+import '../../common/plugins/chart.ts'
+import { ref, computed } from 'vue'
+import store from '../../store'
+import {
+  Chart,
+  BarController,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+} from 'chart.js'
+
+Chart.register(BarController, CategoryScale, LinearScale, BarElement)
+
+const trends = computed(() => store.statisticdata.trends)
+
+const date = trends.value.end_day
+
+const data = computed(() => ({
+  labels: trends.value.dataset?.labels,
+  datasets: [
+    {
+      label: 'Minuten gelernt',
+      data: trends.value.dataset?.values ?? [0],
+    },
+  ],
+}))
+const options = ref({
+  plugins: {
+    legend: {
+      position: 'bottom',
+    },
+  },
+})
 </script>
 
 <template>
   <div>
     <div id="month">
-      <span class="heading-small month">{{
-        date.toLocaleString('default', { month: 'long' }) + ' '
-      }}</span>
-      <span class="heading-small">{{
-        date.toLocaleString('default', { year: 'numeric' })
-      }}</span>
+      <span class="heading-small month">{{ date.format('MMMM') + ' ' }}</span>
+      <span class="heading-small">{{ date.format('YYYY') }}</span>
     </div>
-    <img src="/assets/statisticPlaceholders/diagram.svg" alt="Trend Diagram" />
+    <BarChart
+      :chart-data="data"
+      :options="options"
+      css-classes="chart-container"
+    />
   </div>
 </template>
 
