f3964eadda57bcfc250ac0b5c325612129cf2d00
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-21 15:31:49 +0100

    Update to initialize webcam on mounted

--- frontend/flai_app/src/components/Webcam.vue
+++ frontend/flai_app/src/components/Webcam.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import store from '../store'
 
 const webcamFeed = computed(() => store.webcamdata.webcam.webcamFeed)
 const webcamContainer = ref<HTMLDivElement>()
 
-// TODO: Less hacky way to implement this?
-watch(webcamFeed, () => {
+const initWebcam = () => {
   if (webcamFeed.value) {
     webcamFeed.value.autoplay = true
     webcamFeed.value.id = 'webcam-feed'
     webcamContainer.value?.appendChild(webcamFeed.value)
   }
-})
+}
+
+onMounted(() => initWebcam())
+
+// only nedded in case the webcamFeed changes after mounting the component
+watch(webcamFeed, () => initWebcam())
 </script>
 
 <template>
