0729c600b29a80d175aa54828f70a2631b066f30
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2022-01-28 13:00:03 +0100

    Move all form related into new form component

--- frontend/flai_app/src/views/RegisterPage.vue
+++ frontend/flai_app/src/views/RegisterPage.vue
@@ -1,32 +1,9 @@
 <script setup lang="ts">
-import textInputField from '../components/TextInputField.vue'
-import customCheckbox from '../components/CustomCheckbox.vue'
-import customButton from '../components/CustomButton.vue'
 import IconLoader from '../components/IconLoader.vue'
 import store from '../store'
-import { ref, watchEffect, reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { RegisterUser } from '../store/userdata'
-//import { useRouter } from 'vue-router'
-
-//const router = useRouter()
-
-const defaultTargetTime = '00:20:00'
-
-const user = ref<RegisterUser>({
-  username: '',
-  email: '',
-  password: '',
-  right_handed: true,
-  target_learning_time: defaultTargetTime,
-})
-const leftHanded = ref(!user.value.right_handed)
-
-watchEffect(() => {
-  leftHanded.value = !user.value.right_handed
-})
-watchEffect(() => {
-  user.value.right_handed = !leftHanded.value
-})
+import Form from '../components/Form.vue'
 
 const inputFieldValidation = reactive({
   username: false,
@@ -36,7 +13,7 @@ const inputFieldValidation = reactive({
 
 type inputFieldKey = keyof typeof inputFieldValidation
 
-const errorMessage = reactive([{ message: '', type: '' }])
+const errorMessage = ref<string[]>([])
 const userActions = store.userdata.actions
 const userMethods = store.userdata.methods
 
@@ -47,9 +24,9 @@ function onclick() {
   emit('openLogin')
 }
 
-const submit = async (): Promise<void> => {
-  errorMessage.length = 0
-  const submitUser = { ...user.value }
+const submit = async (user: RegisterUser): Promise<void> => {
+  errorMessage.value.length = 0
+  const submitUser = { ...user }
   const result = await userActions.postNewUser(submitUser)
   if (result?.status === 200) {
     userMethods.changeEmail(submitUser.email)
@@ -59,10 +36,7 @@ const submit = async (): Promise<void> => {
       inputFieldValidation[el as inputFieldKey] = false
     }
     for (let i = 0; i < result?.data.length; i++) {
-      errorMessage.push({
-        message: result?.data[i].message,
-        type: result?.data[i].path[0],
-      })
+      errorMessage.value.push(result?.data[i].message)
       inputFieldValidation[result?.data[i].path[0] as inputFieldKey] = true
     }
   }
@@ -80,74 +54,17 @@ const submit = async (): Promise<void> => {
       <div class="lead-paragraph center-text body-small">
         Registriere dich, um die deutsche Gebärdensprache zu erlernen.
       </div>
-      <div
-        v-for="err in errorMessage"
-        :key="err.message"
-        class="error-message body-small"
-      >
-        {{ err.message }}
-      </div>
-      <form>
-        <text-input-field
-          v-model="user.username"
-          label-name="Benutzername"
-          placeholder="MaxMuster"
-          element-class="default_input_field input-form-primary"
-          :validation-wrong="inputFieldValidation.username"
-        />
-        <text-input-field
-          v-model="user.email"
-          label-name="E-Mail-Adresse"
-          placeholder="maxmusterman@flai.de"
-          element-class="default_input_field input-form-primary"
-          :validation-wrong="inputFieldValidation.email"
-        />
-        <text-input-field
-          v-model="user.password"
-          label-name="Passwort"
-          placeholder="********"
-          element-class="default_input_field input-form-primary"
-          custom-type="password"
-          :validation-wrong="inputFieldValidation.password"
-        />
-        <text-input-field
-          v-model="user.target_learning_time"
-          label-name="Tägliches Lernziel"
-          :placeholder="defaultTargetTime"
-          element-class="default_input_field input-form-primary"
-          custom-type="time"
-          :time-step="1"
-        />
-        <div class="toggle-checkbox">
-          <p class="body-medium">Händigkeit</p>
-          <custom-checkbox
-            v-model="leftHanded"
-            label-name="Links"
-            element-class="primary-checkbox"
-            component-class="primary-checkbox body-small"
-            checkmark-class="checkmark"
-          />
-          <custom-checkbox
-            v-model="user.right_handed"
-            label-name="Rechts"
-            element-class="primary-checkbox"
-            component-class="primary-checkbox body-small"
-            checkmark-class="checkmark"
-          />
-        </div>
-        <div class="button-container">
-          <custom-button
-            label="Registrieren"
-            btnclass="button-form-primary prim_small_button_blue"
-            @button-click="submit"
-          />
-        </div>
-      </form>
-      <div class="divider-line"></div>
-      <div class="bottom-paragraph center-text body-small">
-        Du hast ein Konto?
-        <span class="link" @click="onclick">Melde dich an</span>
-      </div>
+      <Form
+        :error-message="errorMessage"
+        :input-field-validation="inputFieldValidation"
+        submit-type="Registrieren"
+        @submit="submit"
+      />
+    </div>
+    <div class="divider-line"></div>
+    <div class="bottom-paragraph center-text body-small">
+      Du hast ein Konto?
+      <span class="link" @click="onclick">Melde dich an</span>
     </div>
   </div>
 </template>
