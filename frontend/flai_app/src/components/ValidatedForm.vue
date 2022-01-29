9c2f0d47cf3d426afff44ce3a149c928795d9661
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-29 14:34:53 +0100

    Create base validated form component

---
+++ frontend/flai_app/src/components/ValidatedForm.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import customButton from '../components/CustomButton.vue'
+
+defineProps<{
+  errorMessage: string[]
+  submitName: string
+}>()
+
+const emit = defineEmits(['submit'])
+</script>
+
+<template>
+  <div v-for="err in errorMessage" :key="err" class="error-message body-small">
+    {{ err }}
+  </div>
+  <form>
+    <slot name="inputs"></slot>
+    <div class="button-container">
+      <custom-button
+        :label="submitName"
+        btnclass="prim_small_button_blue"
+        @button-click="emit('submit')"
+      />
+      <slot name="additional-buttons"></slot>
+    </div>
+  </form>
+</template>
+<style scoped lang="scss">
+@import '../assets/scss/main.scss';
+</style>
