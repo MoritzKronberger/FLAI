85b7acf2a44c45ff4208ef0b6b93543aff3e1288
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-01-30 11:20:35 +0100

    Add interfaces and types for data returned by express server

--- frontend/flai_app/src/ressources/ts/interfaces.ts
+++ frontend/flai_app/src/ressources/ts/interfaces.ts
@@ -1,3 +1,4 @@
+// TODO: needed?
 export interface DropDown {
   label: string
   value: any
@@ -9,4 +10,55 @@ export enum FeedbackStatus {
   Paused = 'Pausiert',
   NoHandDetected = 'Keine Hand erkannt',
   Detecting = 'Erkenne Gebärde, bitte halten...',
+}
+
+// types and interfaces for data returned by the FLAI REST server
+export interface PostgresEntry {
+  [key: string]: string | number | boolean | null
+}
+
+export type PostgresRow = { [key: string]: string | number | boolean | null }[]
+
+export interface JoiContext {
+  [index: string]: string
+  label: string
+  value: string
+  key: string
+}
+
+export interface DefaultData {
+  [index: string]:
+    | number
+    | string
+    | string[]
+    | PostgresRow
+    | PostgresEntry
+    | JoiContext
+    | null
+    | undefined
+  message: string | null
+}
+
+export interface PostgresData extends DefaultData {
+  status: number
+  ids: PostgresEntry | null
+  rows: PostgresRow | null
+  constraint: string | null
+  pgstate: string | null
+  jwt?: string
+}
+
+export interface JoiEntry extends DefaultData {
+  message: string
+  path: string[]
+  type: string[]
+  context: JoiContext
+}
+
+export type JoiData = JoiEntry[]
+
+export interface ExpressData {
+  [index: string]: number | DefaultData | PostgresData | JoiData | unknown
+  status: number
+  data: unknown | DefaultData | PostgresData | JoiData
 }
