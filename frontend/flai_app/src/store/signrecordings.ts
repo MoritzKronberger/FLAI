6508bb96686f9782d9016aa535d0d06da823fd8c
jelllea <lea.jell@hs-augsburg.de>
2022-02-02 12:40:45 +0100

    Delete method

--- frontend/flai_app/src/store/signrecordings.ts
+++ frontend/flai_app/src/store/signrecordings.ts
@@ -1,5 +1,4 @@
 import { readonly } from 'vue'
-import { Sign } from './signdata'
 export interface SignRecording {
   id: string
   path: string
@@ -10,36 +9,8 @@ export interface SignRecording {
 
 const signRecording: SignRecording[] = []
 
-const methods = {
-  // TODO: this method exists only for frontend purposes and will be swapped with the rest request!
-  createSignRecording(sign: Sign) {
-    const signRecordings = []
-    const signFront: SignRecording = {
-      id: '' + sign.id,
-      path: '',
-      mimetype: '',
-      sign_id: '',
-      perspective: 'front',
-    }
-    const name = sign.name.toUpperCase()
-    signFront.path = `src/assets/signs/vid/${signFront.perspective}/${name}_vid_${signFront.perspective}.webm`
-    const signSide: SignRecording = {
-      id: '' + sign.id,
-      path: 'putUrlHere',
-      mimetype: '',
-      sign_id: '',
-      perspective: 'side',
-    }
-    signSide.path = `src/assets/signs/vid/${signSide.perspective}/${name}_vid_${signSide.perspective}.webm`
-    signRecordings.push(signFront)
-    signRecordings.push(signSide)
-    return signRecordings
-  },
-}
-
 const signRecordingData = {
   signRecording: readonly(signRecording) as SignRecording[],
-  methods,
 }
 
 export default signRecordingData
