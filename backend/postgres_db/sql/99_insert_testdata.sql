833ada87aaace71ff0cbf045f60c0b15915696af
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2022-02-03 12:54:35 +0100

    Update testdata for screen recording

--- backend/postgres_db/sql/99_insert_testdata.sql
+++ backend/postgres_db/sql/99_insert_testdata.sql
@@ -10,7 +10,7 @@ BEGIN;
 -- users (test users based on personas)
 INSERT INTO "user" ("email", "username", "password", "right_handed", "target_learning_time")
 VALUES
-('miriam.weber@email.com', 'Miriam', 'supersecret', TRUE, '60 min'),
+('miriam.weber@email.com', 'Miriam', 'supersecret', TRUE, '20 min'),
 ('katharina_hoff@email.com', 'Katharina', 'supersecret', FALSE, '20 min'),
 ('sabine.toth@email.com', 'Sabine', 'supersecret', TRUE, '15 min');
 
@@ -19,13 +19,8 @@ INSERT INTO "exercise_session" ("user_id", "exercise_id", "start_time", "session
 VALUES
 ((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
  (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
- (CURRENT_TIMESTAMP - INTERVAL '48 hours'), 
-'63 min'
-),
-((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
- (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
  (CURRENT_TIMESTAMP - INTERVAL '20 minutes'), 
-NULL
+'22 min'
 ),
 ((SELECT "id" FROM "user"     WHERE "username"='Katharina'), 
  (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
@@ -40,6 +35,81 @@ NULL
 ((SELECT "id" FROM "user"     WHERE "username"='Katharina'), 
  (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
  (CURRENT_TIMESTAMP - INTERVAL '2 day 18 hours'), 
+'27 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '1 day'), 
+'23 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '2 day'), 
+'29 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '3 day'), 
+'24 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '4 day'), 
+'26 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '5 day'), 
+'12 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '6 day'), 
+'23 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '7 day'), 
+'28 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '8 day'), 
+'32 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '9 day'), 
+'21 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '10 day'), 
+'20 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '11 day'), 
+'30 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '12 day'), 
+'23 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '13 day'), 
+'27 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '14 day'), 
+'20 min'
+),
+((SELECT "id" FROM "user"     WHERE "username"='Miriam'), 
+ (SELECT "id" FROM "exercise" WHERE "name"='Buchstabieren lernen'),
+ (CURRENT_TIMESTAMP - INTERVAL '15 day'), 
 '27 min'
 ),
 ((SELECT "id" FROM "user"     WHERE "username"='Katharina'), 
